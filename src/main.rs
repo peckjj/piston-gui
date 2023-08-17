07b3a616028e0bc07a9e871b8b987dd6cea78baa
Josh Peck <jpeck@amctechnology.com>
2023-08-17 15:53:41 -0400

    Formatted and added comments

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
-const NUM_RECTS: usize = 100;
-const RECT_PADDING: u64 = 3;
-const MAX_HEIGHT_RATIO: f64 = 0.85;
-const MAX_PERIODS: u64 = 6;
+const NUM_RECTS: usize = 4000; // Controls number of rects, similar to resolution
+const RECT_PADDING: u64 = 0; // Controls padding between rectangles
+const MAX_HEIGHT_RATIO: f64 = 1.0; // Controls how tall the waves get
+const MAX_PERIODS: u64 = 1; // Controls how many waves there are
+const SPEED_FACTOR: f64 = 12.0; // Controls how fast the waves move
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
@@ -26,6 +27,7 @@ impl App {
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let window_width = args.window_size[0];
+        // Calculate width of rect as (TotalWidthOfWindow - TotalWidthOfPadding) / NUM_RECTS
         let rect_width =
             (window_width - (RECT_PADDING * (NUM_RECTS as u64 + 1)) as f64) / NUM_RECTS as f64;
 
@@ -33,18 +35,27 @@ impl App {
             // Clear the screen.
             clear(GREEN, gl);
 
-            // Draw a box rotating around the middle of the screen.
+            // Draw all rectangles
             let mut i = 0;
 
             while i < NUM_RECTS {
+                // Calculate bottom corner:
+                //  x = i * Width_Of_Rectangle + Total_Width_Of_Padding_To_The_Left
+                //  y = Constant
                 let bottom_x = (i as f64 * rect_width) + (RECT_PADDING as f64 * (i + 1) as f64);
                 let bottom_y = args.window_size[1] - 1.0;
 
+                // Calculate top corner:
+                //  x = bottom_x + Width_Of_Rectangle
+                //  y  = Bottom_Of_Screen - (MAX_HEIGHT_RATIO * <% of rotation out of 360 degrees> * Bottom_Of_Screen)
                 let top_x = bottom_x + rect_width;
-                let top_y: f64 = args.window_size[1] - (MAX_HEIGHT_RATIO * args.window_size[1] * rad2ratio(calc_radians(i, self.rotation, MAX_PERIODS)));
+                let top_y: f64 = args.window_size[1]
+                    - (MAX_HEIGHT_RATIO
+                        * args.window_size[1]
+                        * rad2ratio(calc_radians(i, self.rotation, MAX_PERIODS)));
 
-                let square = rectangle::rectangle_by_corners(bottom_x, bottom_y, top_x, top_y);
-                rectangle(RED, square, c.transform, gl);
+                let rect = rectangle::rectangle_by_corners(bottom_x, bottom_y, top_x, top_y);
+                rectangle(RED, rect, c.transform, gl);
                 i += 1;
             }
         });
@@ -52,26 +63,32 @@ impl App {
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        self.rotation += SPEED_FACTOR * args.dt;
     }
 }
 
 fn calc_radians(position: usize, start_rotation: f64, max_rotations: u64) -> f64 {
     let mut radians = 0.0;
 
+    // Calculate maximum radians as max_rotations * 2*PI
     let total_radians: f64 = 2.0 * std::f64::consts::PI * (max_rotations as f64);
+
+    // Calculate rotation interval between rectangles as total_radians / NUM_RECTS
     let rotation_interval = total_radians / (NUM_RECTS as f64);
 
+    // Calculate radians as position * rotation_interval + start_rotation
     radians = (position as f64 * rotation_interval) + start_rotation;
 
     return radians;
 }
 
 fn rad2ratio(radians: f64) -> f64 {
+    // Convert radians to value between -1 and 1. Map result to range between 0 and 1.
     let ratio = map_range((-1.0, 1.0), (0.0, 1.0), radians.cos());
     return ratio;
 }
 
+// Converts a float in <from_range> to a float in <to_range>
 fn map_range(from_range: (f64, f64), to_range: (f64, f64), s: f64) -> f64 {
     to_range.0 + (s - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
 }
